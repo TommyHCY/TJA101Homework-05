42988c40591e106ece663b7c1b4101255b80244c
TommyHCY <chienyu749@gmail.com>
2025-03-31 08:57:18 +0800

    方法封裝完成

--- src/hw3/LottoNum.java
+++ src/hw3/LottoNum.java
@@ -9,14 +9,77 @@ public class LottoNum {
 		int[] lottoNum = new int[49];
 		int[] saveNum = new int[49];
 		int count = 0;
-		
+
 		for (int i = 0; i < lottoNum.length; i++) {
 			lottoNum[i] = i + 1;
 		}
 		Scanner sc = new Scanner(System.in);
 		System.out.println("阿文...請輸入你討厭哪個數字?");
 		int hateNum = sc.nextInt();
+		int[] savedNum = chosenNum(hateNum, lottoNum);
+		printChosenNum(savedNum);
+		printRandomNum(savedNum, 6);
+//		for (int i = 0; i < lottoNum.length; i++) {
+//			if (lottoNum[i] % 10 == hateNum || lottoNum[i] / 10 == hateNum) {
+//				continue;
+//			} else {
+//				count++;
+//				saveNum[i] = i + 1;
+//			}
+//		}
+//		Arrays.sort(saveNum);
+//		count = 0;
+//		for (int i = 0; i < saveNum.length; i++) {
+//			if (saveNum[i] != 0) {
+//				break;
+//			}
+//			count++;
+//		}
+//		saveNum = Arrays.copyOfRange(saveNum, count, 49);
 
+//		count = 0;
+//		for (int i = 0; i < saveNum.length; i++) {
+//			count++;
+//			if (count % 6 == 0) {
+//				System.out.print(saveNum[i] + "\t");
+//				System.out.println();
+//			} else {
+//				System.out.print(saveNum[i] + "\t");
+//			}
+//		}
+//		System.out.print("總共有" + saveNum.length + "數字可選");
+//		System.out.println("\n" + "=================================");
+//		int[] ranIndex = new int[6];
+////		System.out.println(ranIndex.length);
+//
+//		for (int i = 0; i < ranIndex.length; i++) {
+//			ranIndex[i] = (int) (Math.random() * saveNum.length) ;
+//		}
+////		int[] ranIndex = { 10, 32, 5, 32, 6, 9 };
+//		repeat: while (true) {
+//			// 作ranIdex判斷是否有重複
+//			for (int i = 0; i < ranIndex.length; i++) {
+//				for (int j = i + 1; j < ranIndex.length; j++) {
+//					if (ranIndex[i] == ranIndex[j]) {
+//						ranIndex[i] = (int) (Math.random() * saveNum.length) ;
+//						continue repeat;
+//					} else {
+//						continue;
+//					}
+//				}
+//			}
+//			break;
+//		}
+//		for (int i = 0; i < ranIndex.length; i++) {
+//			System.out.print(saveNum[ranIndex[i]] + "\t");
+//		}
+
+	}
+
+	// TODO 封裝方法
+	public static int[] chosenNum(int hateNum, int[] lottoNum) {
+		int count = 0;
+		int[] saveNum = new int[49];
 		for (int i = 0; i < lottoNum.length; i++) {
 			if (lottoNum[i] % 10 == hateNum || lottoNum[i] / 10 == hateNum) {
 				continue;
@@ -34,23 +97,30 @@ public class LottoNum {
 			count++;
 		}
 		saveNum = Arrays.copyOfRange(saveNum, count, 49);
-		count = 0;
-		for (int i = 0; i < saveNum.length; i++) {
+		return saveNum;
+	}
+
+	public static void printChosenNum(int[] num) {
+		int count = 0;
+		for (int i = 0; i < num.length; i++) {
 			count++;
 			if (count % 6 == 0) {
-				System.out.print(saveNum[i] + "\t");
+				System.out.print(num[i] + "\t");
 				System.out.println();
 			} else {
-				System.out.print(saveNum[i] + "\t");
+				System.out.print(num[i] + "\t");
 			}
 		}
-		System.out.print("總共有" + saveNum.length + "數字可選");
+		System.out.print("總共有" + num.length + "數字可選");
 		System.out.println("\n" + "=================================");
-		int[] ranIndex = new int[6];
+	}
+
+	public static void printRandomNum(int[] nums, int num) {
+		int[] ranIndex = new int[num];
 //		System.out.println(ranIndex.length);
 
 		for (int i = 0; i < ranIndex.length; i++) {
-			ranIndex[i] = (int) (Math.random() * saveNum.length) ;
+			ranIndex[i] = (int) (Math.random() * nums.length);
 		}
 //		int[] ranIndex = { 10, 32, 5, 32, 6, 9 };
 		repeat: while (true) {
@@ -58,7 +128,7 @@ public class LottoNum {
 			for (int i = 0; i < ranIndex.length; i++) {
 				for (int j = i + 1; j < ranIndex.length; j++) {
 					if (ranIndex[i] == ranIndex[j]) {
-						ranIndex[i] = (int) (Math.random() * saveNum.length) ;
+						ranIndex[i] = (int) (Math.random() * nums.length);
 						continue repeat;
 					} else {
 						continue;
@@ -68,8 +138,7 @@ public class LottoNum {
 			break;
 		}
 		for (int i = 0; i < ranIndex.length; i++) {
-			System.out.print(saveNum[ranIndex[i]] + "\t");
+			System.out.print(nums[ranIndex[i]] + "\t");
 		}
-
 	}
 }
